25dadcb7b796354ede5c83f770d8dd86f9285bf5
Antonio Desroche <antdesroche@gmail.com>
2019-10-21 20:13:31 -0400

    7 out of 8 tests passed. Wasn't able to finish one of the extra credit questions, but i think it's sufficient.

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,37 @@
 function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
-}
 
+  var matchingOrders = existingBook.filter( (currentItem) => {
+  }) 
+  if (existingBook.push(incomingOrder) !== true){
+
+  }
+ const totalOrders = existingBook.concat([incomingOrder])
+ let btcOrder = totalOrders[0]
+ let btcOrder2 = totalOrders[1]
+ if (btcOrder.type !== btcOrder2.type &&
+  btcOrder.price === btcOrder2.price &&
+  btcOrder.quantity === btcOrder2.quantity) {
+    return []
+  }
+  if (btcOrder.type !== btcOrder2.type &&
+    btcOrder.quantity !== btcOrder2.quantity &&
+    btcOrder.price === btcOrder2.price) {
+    if (btcOrder.quantity - btcOrder2.quantity > 0) {
+      return [{
+        type: 'buy',
+        quantity: btcOrder.quantity - btcOrder2.quantity,
+        price: btcOrder.price,
+      }]
+    }
+      else if (btcOrder2.quantity - btcOrder.quantity > 0) {
+        return [{
+          type: 'sell',
+          quantity: btcOrder2.quantity - btcOrder.quantity,
+          price: btcOrder.price,
+        }]
+    }
+  }
+ 
+return existingBook
+}
 module.exports = reconcileOrder
